55310b32ecd4b844d704409c24da371fa4821eb9
ousadojunplid <ousadojunplid@gmail.com>
2023-06-17 11:46:14 -0300

    implement router

--- app/src/server/routers/v1/user/create/index.ts
+++ app/src/server/routers/v1/user/create/index.ts
@@ -1,7 +1,34 @@
 import { Router } from "express";
+import { createMessageController } from "../../../../../application/heart/Message/create";
+import { Joi, validate } from "express-validation";
+import { verifyTokenAcessGlobal } from "../../../../../middleware/Auth/VerifyTokenGlobal";
 
 const router = Router();
 
-router.post("");
+router.post(
+  "/message",
+  verifyTokenAcessGlobal().execute,
+  validate({
+    body: Joi.object({
+      text: Joi.string().required().messages({
+        "string.empty": "Campo obrigatório",
+        "any.required": "Campo obrigatório",
+        "string.base": "Este campo precisa ser uma string",
+      }),
+      user_key: Joi.string().max(55).required().messages({
+        "string.empty": "Campo obrigatório",
+        "any.required": "Campo obrigatório",
+        "string.base": "Este campo precisa ser uma string",
+        "string.max": "O e-mail precisa ter no máximo 55 caracteres",
+      }),
+      days: Joi.number().min(0).required().messages({
+        "any.empty": "Campo obrigatório",
+        "any.required": "Campo obrigatório",
+        "number.min": "Este campo precisar ser maior ou igual a 0",
+      }),
+    }),
+  }),
+  createMessageController
+);
 
 export { router as RouterCreate };
